5eb70f37c2d0698b6c97f838e1904f9b20819cb3
Lee Jones <lee@dragonflyrg.com>
2024-10-25 07:59:28 -0500

    Added slack formatter.

---
+++ src/Log/Format/Slack.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Neuron\Log\Format;
+
+use Neuron\Log;
+use Neuron\Log\Format\IFormat;
+
+class Slack implements IFormat
+{
+	protected function getContextString( array $ContextList ) : string
+	{
+		$Context = '';
+
+		foreach( $ContextList as $Name => $Value )
+		{
+			if( strlen( $Context ) )
+			{
+				$Context .= '|';
+			}
+
+			$Context .= "$Name=$Value";
+		}
+
+		if( $Context )
+		{
+			return $Context;
+		}
+
+		return "";
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function format( Log\Data $Data ): string
+	{
+		$Output = date( "[Y-m-d G:i:s]", $Data->TimeStamp );
+
+		$Context = $this->getContextString( $Data->Context );
+
+		$Output .= " *$Data->LevelText* ";
+
+		if( $Context )
+			$Output .= "_({$this->getContextString( $Data->Context )})_";
+
+		return $Output." `".$Data->Text."`";
+	}
+}
