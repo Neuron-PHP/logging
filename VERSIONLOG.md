7fbc27438d7e9c7882307e4e06edec065bc2a922
Lee Jones <lee@dragonflyrg.com>
2025-01-11 12:23:02 -0600

    updated the date field in the json format.

--- VERSIONLOG.md
+++ VERSIONLOG.md
@@ -1,5 +1,6 @@
 ## 0.7.8
 * Added StdOutStdError destination.
+* Updated documentation.
 
 ## 0.7.7
 ## 0.7.6 2024-12-16
--- src/Log/Format/JSON.php
+++ src/Log/Format/JSON.php
@@ -17,7 +17,7 @@ class JSON implements IFormat
 	public function format( Log\Data $Data ) : string
 	{
 		$aData = [
-			'date'	=> date( "[Y-m-d G:i:s]", $Data->TimeStamp ),
+			'date'	=> date( "Y-m-d G:i:s", $Data->TimeStamp ),
 			'level'	=> $Data->LevelText,
 			'text'	=> $Data->Text
 		];
